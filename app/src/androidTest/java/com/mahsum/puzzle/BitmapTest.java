eb1078386e9050d728f3b469bcd52f25aa02f653
mahsumdemir <mahsumdemir93mdmd@gmail.com>
2017-02-27 00:16:03 +0300

    Add tests for joining of pieces to create original image

--- app/src/androidTest/java/com/mahsum/puzzle/BitmapTest.java
+++ app/src/androidTest/java/com/mahsum/puzzle/BitmapTest.java
@@ -1,11 +1,17 @@
 package com.mahsum.puzzle;
 
+import static android.graphics.BitmapFactory.decodeResource;
+import static com.mahsum.puzzle.Saving.saveBitmap;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.test.InstrumentationRegistry;
 import com.mahsum.puzzle.util.Util;
 import org.junit.Before;
@@ -22,24 +28,60 @@ public class BitmapTest {
 
   @Test
   public void testBitmapsEqual() throws Exception {
-    Bitmap bitmap = BitmapFactory.decodeResource(mTargetContext.getResources(),
+    Bitmap bitmap = decodeResource(mTargetContext.getResources(),
                                                  R.drawable.harput);
-    Bitmap bitmap2 = BitmapFactory.decodeResource(mTargetContext.getResources(),
+    Bitmap bitmap2 = decodeResource(mTargetContext.getResources(),
                                                   R.drawable.harput);
-    Util.assertEquals(bitmap, bitmap2);
+    Util.assertBitmapsEquals(bitmap, bitmap2);
   }
 
   @Test
   public void testBitmapsEqual_ShouldFail() throws Exception {
-    Bitmap bitmap = BitmapFactory.decodeResource(mTargetContext.getResources(),
+    Bitmap bitmap = decodeResource(mTargetContext.getResources(),
                                                  R.drawable.harput);
-    Bitmap bitmap2 = BitmapFactory.decodeResource(mTargetContext.getResources(),
+    Bitmap bitmap2 = decodeResource(mTargetContext.getResources(),
                                                   R.drawable.harput_modified);
     try {
-      Util.assertEquals(bitmap, bitmap2);
+      Util.assertBitmapsEquals(bitmap, bitmap2);
       fail("Images should be different.");
     }catch (RuntimeException e){
       //Ignored.
     }
+  }
+
+  @Test
+  public void testJoinBitmaps_ShouldFail() throws Exception {
+    String dir = BuildConfig.IMAGES_ROOT_DIR + "/harput_900x900";
+    Bitmap original = BitmapFactory.decodeFile(dir + "/original.png");
+    Bitmap[] pieces = readPieces(dir);
+
+    Bitmap joinedBitmap = Bitmap.createBitmap(original.getWidth(),
+                                              original.getHeight(),
+                                              Config.ARGB_8888);
+
+    Canvas canvas = new Canvas(joinedBitmap);
+    joinAll(pieces, canvas);
+
+    Util.assertBitmapsEquals(original, joinedBitmap);
+  }
+
+  private void joinAll(Bitmap[] pieces, Canvas canvas) {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    int index = 0;
+    for (int y = 0, height = 0; y < 3; y++, height += 300 ){
+      for (int x = 0, width = 0; x < 3; x++, width += 300){
+        canvas.drawBitmap(pieces[index], width, height, paint);
+        index++;
+      }
+
+    }
+  }
+
+  private Bitmap[] readPieces(String dir) {
+    Bitmap[] pieces = new Bitmap[9];
+    for (int i = 0; i < 9 ; i++){
+      pieces[i] = BitmapFactory.decodeFile(dir + "/" + String.valueOf(i) + ".png");
+    }
+    return pieces;
   }
 }
--- app/src/main/java/com/mahsum/puzzle/util/Util.java
+++ app/src/main/java/com/mahsum/puzzle/util/Util.java
@@ -1,12 +1,15 @@
 package com.mahsum.puzzle.util;
 
 
+import static com.mahsum.puzzle.Saving.saveBitmap;
 import static junit.framework.Assert.fail;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import com.mahsum.puzzle.PuzzleFactory;
 import com.mahsum.puzzle.PuzzleFactory.TYPE;
+import com.mahsum.puzzle.exceptions.FileCouldNotCreated;
+import com.mahsum.puzzle.exceptions.FileCouldNotSaved;
 import com.mahsum.puzzle.makepuzzle.BitmapMask;
 
 public class Util {
@@ -17,7 +20,7 @@ public class Util {
     return new Bitmap[5];
   }
 
-  public static void assertEquals(Bitmap bitmap, Bitmap bitmap2) {
+  public static void assertBitmapsEquals(Bitmap bitmap, Bitmap bitmap2) {
     if (bitmap == bitmap2) return;
     if (bitmap.getHeight() != bitmap2.getHeight() || bitmap.getWidth() != bitmap2.getWidth()) {
       throw new RuntimeException("Heights of bitmaps are different");
@@ -25,10 +28,21 @@ public class Util {
     for(int height = 0; height < bitmap.getHeight(); height++){
       for(int width = 0; width < bitmap.getWidth(); width++){
         if (bitmap.getPixel(width, height) != bitmap2.getPixel(width, height)){
-          throw new RuntimeException(
-              String.format("Pixels at (%d, %d) of bitmaps are different", width, height));
+          printErrorMessage(bitmap, bitmap2, height, width);
         }
       }
     }
+  }
+
+  private static void printErrorMessage(Bitmap bitmap, Bitmap bitmap2, int height, int width) {
+    String path = "/storage/sdcard/puzzle/error/";
+    String errorMessage = String.format("Pixels at (%d, %d) of bitmaps are different", width, height);
+    try {
+      saveBitmap(bitmap, path + "expected");
+      saveBitmap( bitmap2, path + "found");
+    }catch (FileCouldNotCreated | FileCouldNotSaved e){
+      errorMessage += " Expected and found bitmaps could not saved on device.";
+    }
+    throw new RuntimeException(errorMessage);
   }
 }
