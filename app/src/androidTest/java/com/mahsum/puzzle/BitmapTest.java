cebadeae48d0d865a130f73aebceec2a493303a4
mahsumdemir <mahsumdemir93mdmd@gmail.com>
2017-03-01 12:25:04 +0300

    Delete untested trash codes.

--- app/src/androidTest/java/com/mahsum/puzzle/BitmapTest.java
+++ app/src/androidTest/java/com/mahsum/puzzle/BitmapTest.java
@@ -1,25 +1,11 @@
 package com.mahsum.puzzle;
 
-import static android.graphics.BitmapFactory.decodeByteArray;
 import static android.graphics.BitmapFactory.decodeResource;
-import static com.mahsum.puzzle.Saving.saveBitmap;
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.PorterDuff.Mode;
-import android.graphics.PorterDuffXfermode;
 import android.support.test.InstrumentationRegistry;
-import com.mahsum.puzzle.PuzzleFactory.TYPE;
-import com.mahsum.puzzle.exceptions.FileCouldNotCreated;
-import com.mahsum.puzzle.exceptions.FileCouldNotSaved;
 import com.mahsum.puzzle.util.Util;
 import org.junit.Before;
 import org.junit.Test;
@@ -55,33 +41,4 @@ public class BitmapTest {
       //Ignored.
     }
   }
-
-  @Test
-  public void testJoinBitmaps_ShouldFail() throws Exception {
-    Puzzle puzzle = new Puzzle();
-    Bitmap original = BitmapFactory.decodeFile(BuildConfig.IMAGES_ROOT_DIR +
-                                                   "/harput_900x900/original.png");
-    puzzle.setOriginalBitmap(original);
-    puzzle.setType(TYPE.FOUR_X_FOUR);
-    puzzle.createPuzzle();
-
-    Bitmap joinedBitmap = Bitmap.createBitmap(original.getWidth(), original.getHeight(), Config.ARGB_8888);
-    joinAll(puzzle.getPieces(), new Canvas(joinedBitmap));
-
-    Util.assertBitmapsEquals(original, joinedBitmap);
-  }
-
-  private void joinAll(Bitmap[] pieces, Canvas canvas) {
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    int index = 0;
-    for (int y = 0, height = 0; y < 3; y++, height += 300 ){
-      for (int x = 0, width = 0; x < 3; x++, width += 300){
-        canvas.drawBitmap(pieces[index], width, height, paint);
-        index++;
-      }
-
-    }
-  }
-
-
 }
--- app/src/main/java/com/mahsum/puzzle/util/Util.java
+++ app/src/main/java/com/mahsum/puzzle/util/Util.java
@@ -2,24 +2,12 @@ package com.mahsum.puzzle.util;
 
 
 import static com.mahsum.puzzle.Saving.saveBitmap;
-import static junit.framework.Assert.fail;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import com.mahsum.puzzle.PuzzleFactory;
-import com.mahsum.puzzle.PuzzleFactory.TYPE;
 import com.mahsum.puzzle.exceptions.FileCouldNotCreated;
 import com.mahsum.puzzle.exceptions.FileCouldNotSaved;
-import com.mahsum.puzzle.makepuzzle.BitmapMask;
 
 public class Util {
-  public static Bitmap[] imageToPuzzle(Context context, Bitmap image){
-    image = Bitmap.createScaledBitmap(image, 1200, 1200, false);
-    int maskHeight = 300, maskWeight = 300;
-    PuzzleFactory.createPuzzle(image, BitmapMask.getAll(context, maskWeight, maskHeight), TYPE.FOUR_X_FOUR);
-    return new Bitmap[5];
-  }
-
   public static void assertBitmapsEquals(Bitmap bitmap, Bitmap bitmap2) {
     if (bitmap == bitmap2) return;
     if (bitmap.getHeight() != bitmap2.getHeight() || bitmap.getWidth() != bitmap2.getWidth()) {
