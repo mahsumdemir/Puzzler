007ec3a038dd634d413e0ccb2d978fcdc4feaa1e
mahsumdemir <mahsumdemir93mdmd@gmail.com>
2017-03-12 18:43:09 +0300

    Add GameBoard and GameBoardTest.

--- app/src/androidTest/java/com/mahsum/puzzle/ui/GameBoardTest.java
+++ app/src/androidTest/java/com/mahsum/puzzle/ui/GameBoardTest.java
@@ -2,21 +2,25 @@ package com.mahsum.puzzle.ui;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasImeAction;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.mahsum.puzzle.Saving.saveBitmap;
 import static junit.framework.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.fail;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 import com.mahsum.puzzle.BuildConfig;
+import com.mahsum.puzzle.exceptions.FileCouldNotCreated;
+import com.mahsum.puzzle.exceptions.FileCouldNotSaved;
+import com.mahsum.puzzle.utility.Util;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,44 +28,93 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class GameBoardTest {
-  private Context targetApplicationContext;
-  private Bitmap image;
-  private final int RESOLUTION_X = 900, RESOLUTION_Y = 900;
-  private final int PIECES_X = 3, PIECES_Y = 3;
 
   @Rule
-  public ActivityTestRule<GameBoard> gameBoardActivityTestRule =
-      new ActivityTestRule<>(GameBoard.class, true, false);
+  public ActivityTestRule<GameBoard> gameBoardActivityTestRule = new ActivityTestRule<>(
+      GameBoard.class, true, false);
 
   @Before
   public void setUp() throws Exception {
-    targetApplicationContext = InstrumentationRegistry.getContext();
-    image = BitmapFactory.decodeFile(BuildConfig.IMAGES_ROOT_DIR + "/harput.png");
+    final String PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    if (!Util.grantPermission(InstrumentationRegistry.getTargetContext(), PERMISSION)) {
+      fail("Error. Could not grand permission: " + PERMISSION);
+    }
+  }
+
+  @Test
+  public void testActivityReceivedData() throws Exception {
+    //set up
+    String imageFilePath = BuildConfig.IMAGES_ROOT_DIR + "/harput.png";
+    int resolutionX = 900, resolutionY = 900;
+    int piecesX = 3, piecesY = 3;
+    Intent intent = createIntentWithExtras(imageFilePath, resolutionX, resolutionY, piecesX,
+                                           piecesY);
+    gameBoardActivityTestRule.launchActivity(intent);
+
+    //assert
+    GameBoard gameBoard = gameBoardActivityTestRule.getActivity();
+    assertEquals(imageFilePath, gameBoard.getImageFilePath());
+    assertEquals(resolutionX, gameBoard.getResolutionX());
+    assertEquals(resolutionY, gameBoard.getResolutionY());
+    assertEquals(piecesX, gameBoard.getPiecesX());
+    assertEquals(piecesY, gameBoard.getPiecesY());
+  }
+
+  private Intent createIntentWithExtras(String imageFilePath, int resolutionX, int resolutionY,
+                                        int piecesX, int piecesY) {
+
+    Context targetApplicationContext = InstrumentationRegistry.getContext();
 
     Intent intent = new Intent(targetApplicationContext, GameBoard.class);
-    intent.putExtra("ORIGINAL_IMAGE", image);
-    intent.putExtra("RESOLUTION_X", RESOLUTION_X);
-    intent.putExtra("RESOLUTION_Y", RESOLUTION_Y);
-    intent.putExtra("PIECES_X", PIECES_X);
-    intent.putExtra("PIECES_Y", PIECES_Y);
+    intent.putExtra("ORIGINAL_IMAGE_FILE_PATH", imageFilePath);
+    intent.putExtra("RESOLUTION_X", resolutionX);
+    intent.putExtra("RESOLUTION_Y", resolutionY);
+    intent.putExtra("PIECES_X", piecesX);
+    intent.putExtra("PIECES_Y", piecesY);
+    return intent;
+  }
+
+  @Test
+  public void testUserSeesPuzzlePieces_MultiplePuzzleType() throws Exception {
+    testUserSeesPuzzlePieces(900, 900, 3, 3);
+    testUserSeesPuzzlePieces(600, 600, 2, 2);
+    testUserSeesPuzzlePieces(600, 600, 3, 3);
+  }
+
+  private void testUserSeesPuzzlePieces(int resolutionX, int resolutionY, int piecesX, int piecesY)
+      throws FileCouldNotSaved, FileCouldNotCreated {
+    //set up
+    String imageFilePath = BuildConfig.IMAGES_ROOT_DIR + "/harput.png";
+    Intent intent = createIntentWithExtras(imageFilePath, resolutionX, resolutionY, piecesX,
+                                           piecesY);
+    //exercise
     gameBoardActivityTestRule.launchActivity(intent);
-  }
 
-  @Test
-  public void testReceiveData() throws Exception {
-    GameBoard gameBoard = gameBoardActivityTestRule.getActivity();
-    assertEquals(gameBoard.getImage(), image);
-  }
+    //take Screen shot for examination
+    String ssName =
+        BuildConfig.IMAGES_ROOT_DIR + "/harput" + String.valueOf(resolutionX) + "x" + String
+            .valueOf(resolutionY) + "_" + String.valueOf(piecesX) + "x" + String.valueOf(piecesY);
+    takeScreenShot(gameBoardActivityTestRule.getActivity(), ssName);
 
-  @Test
-  public void testUserSeesPuzzlePieces() throws Exception {
     //assert
-    assertPiecesDisplayed(9);
+    assertPiecesDisplayed(gameBoardActivityTestRule.getActivity(), piecesX * piecesY);
+
+    gameBoardActivityTestRule.getActivity().finish();
   }
 
-  private void assertPiecesDisplayed(int piecesNumber) {
+  private void assertPiecesDisplayed(GameBoard gameBoard, int piecesNumber)
+      throws FileCouldNotSaved, FileCouldNotCreated {
+    //assert
     for (int index = 0; index < piecesNumber; index++) {
-      onView(withId(1)).check((matches(isDisplayed())));
+      int id = gameBoard.getImageViewIdByIndex(index);
+      onView(withId(id)).check((matches(isDisplayed())));
     }
+  }
+
+  private void takeScreenShot(Activity activity, String ssName) throws FileCouldNotSaved, FileCouldNotCreated {
+    View rootView = activity.getWindow().getDecorView().getRootView();
+    rootView.setDrawingCacheEnabled(true);
+    Bitmap bitmap = rootView.getDrawingCache();
+    saveBitmap(bitmap, ssName);
   }
 }
--- app/src/main/java/com/mahsum/puzzle/ui/GameBoard.java
+++ app/src/main/java/com/mahsum/puzzle/ui/GameBoard.java
@@ -1,23 +1,100 @@
 package com.mahsum.puzzle.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.GridLayout;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.ImageView;
+import com.mahsum.puzzle.Piece;
+import com.mahsum.puzzle.Puzzle;
 import com.mahsum.puzzle.R;
+import com.mahsum.puzzle.Type;
 
 
 public class GameBoard extends Activity {
 
   private Bitmap image;
+  private String imageFilePath;
+  private int resolutionX;
+  private int resolutionY;
+  private int piecesX;
+  private int piecesY;
+  private ImageView[] views;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.game_board);
+    parseIntent(getIntent());
+    Piece[] pieces = createPuzzle();
+    initBoard(pieces);
+  }
+
+  private Piece[] createPuzzle() {
+    Puzzle puzzle = new Puzzle(new Type(piecesX, piecesY));
+    Bitmap image = BitmapFactory.decodeFile(imageFilePath);
+    image = Bitmap.createScaledBitmap(image, resolutionX, resolutionY, false);
+    puzzle.setImage(image);
+    return puzzle.createPuzzle();
+  }
+
+  private void initBoard(Piece[] pieces) {
+    views = new ImageView[piecesX * piecesY];
+
+    GridLayout gridLayout = (GridLayout) findViewById(R.id.pieceBoard);
+    gridLayout.setRowCount(piecesY);
+    gridLayout.setColumnCount(piecesX);
+    for (int index = 0; index < views.length; index++){
+      views[index] = initImageView(pieces[index].getBitmap());
+      gridLayout.addView(views[index]);
+    }
+  }
+
+  private ImageView initImageView(Bitmap image) {
+    ImageView view = new ImageView(getApplicationContext());
+    view.setId(ImageView.generateViewId());
+    view.setImageBitmap(image);
+    LayoutParams layoutParams = new LayoutParams();
+    layoutParams.height = LayoutParams.WRAP_CONTENT;
+    layoutParams.width = LayoutParams.WRAP_CONTENT;
+    view.setLayoutParams(layoutParams);
+    return view;
+  }
+
+  private void parseIntent(Intent intent) {
+    imageFilePath = intent.getStringExtra("ORIGINAL_IMAGE_FILE_PATH");
+    resolutionX = intent.getIntExtra("RESOLUTION_X", 0);
+    resolutionY = intent.getIntExtra("RESOLUTION_Y", 0);
+    piecesX = intent.getIntExtra("PIECES_X", 0);
+    piecesY = intent.getIntExtra("PIECES_Y", 0);
   }
 
   public Bitmap getImage() {
     return image;
+  }
+
+  public String getImageFilePath() { return imageFilePath; }
+  public int getResolutionX() { return resolutionX; }
+
+
+  public int getResolutionY() {
+    return resolutionY;
+  }
+
+  public int getPiecesX() {
+    return piecesX;
+  }
+
+  public int getPiecesY() {
+    return piecesY;
+  }
+
+  public int getImageViewIdByIndex(int index) {
+    return views[index].getId();
   }
 }
