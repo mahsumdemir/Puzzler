2cf08ef6b8fe36c9e30f15b1f312d0d6a0048abe
mahsumdemir <mahsumdemir93mdmd@gmail.com>
2017-04-03 02:52:23 +0300

    Refactor some fields by using ButterKnife and RxJava and RxBinding.

--- app/src/main/java/com/mahsum/puzzle/ui/PieceImageView.java
+++ app/src/main/java/com/mahsum/puzzle/ui/PieceImageView.java
@@ -5,16 +5,19 @@ import android.content.Context;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.Log;
 import android.view.DragEvent;
-import android.view.View;
-import android.view.View.OnLongClickListener;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.mahsum.puzzle.Piece;
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
-public class PieceImageView extends AppCompatImageView implements OnLongClickListener{
+public class PieceImageView extends AppCompatImageView{
   private Piece piece;
   private int pieceArrayIndex;
   private int viewArrayIndex;
+  private Observable<Object> longClickSource;
 
   public PieceImageView(Context context) {
     super(context);
@@ -29,7 +32,47 @@ public class PieceImageView extends AppCompatImageView implements OnLongClickLis
     setX(piece, piecesX, index);
     setY(piece, piecesY, index);
     setImageBitmap(piece.getBitmap());
-    setOnLongClickListener(this);
+
+    setUpLongClickStream();
+    setUpDragAndDropStream();
+  }
+
+  private void setUpDragAndDropStream() {
+    Observable<DragEvent> dragEventStream = RxView.drags(this);
+    dragEventStream.subscribe(new Consumer<DragEvent>() {
+      @Override
+      public void accept(@NonNull DragEvent event) throws Exception {
+        final int action = event.getAction();
+
+        // Handles each of the expected events
+        switch(action) {
+          case DragEvent.ACTION_DROP:
+
+            ClipData data = event.getClipData();
+            String arrayIndex = (String) data.getItemAt(0).getText();
+            PieceViewList.swapContents(Integer.valueOf(arrayIndex), PieceImageView.this.viewArrayIndex);
+            // Returns true. DragEvent.getResult() will return true.
+            break;
+
+          // An unknown action type was received.
+          default:
+            Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+            break;
+        }
+      }
+    });
+  }
+
+  private void setUpLongClickStream() {
+    longClickSource = RxView.longClicks(this);
+    Consumer shadowDrawer = new Consumer() {
+      @Override
+      public void accept(@NonNull Object o) throws Exception {
+        ClipData dragData = ClipData.newPlainText(null, String.valueOf(viewArrayIndex));
+        startDrag(dragData, new ImageView.DragShadowBuilder(PieceImageView.this), viewArrayIndex, 0);
+      }
+    };
+    longClickSource.subscribe(shadowDrawer);
   }
 
   private void setY(Piece piece, int piecesY, int index){
@@ -63,63 +106,6 @@ public class PieceImageView extends AppCompatImageView implements OnLongClickLis
   public void setPiece(Piece piece){
     this.piece = piece;
     setImageBitmap(piece.getBitmap());
-  }
-
-
-  @Override
-  public boolean onLongClick(View v) {
-    ClipData dragData = ClipData.newPlainText(null, String.valueOf(viewArrayIndex));
-    startDrag(dragData, new ImageView.DragShadowBuilder(this), null, 0);
-    return true;
-  }
-
-  @Override
-  public boolean onDragEvent(DragEvent event) {
-    // Defines a variable to store the action type for the incoming event
-    final int action = event.getAction();
-
-    // Handles each of the expected events
-    switch(action) {
-
-      case DragEvent.ACTION_DRAG_STARTED:
-
-        // returns true to indicate that the View can accept the dragged data.
-        return true;
-
-      case DragEvent.ACTION_DRAG_ENTERED:
-
-        return true;
-
-      case DragEvent.ACTION_DRAG_LOCATION:
-
-        // Ignore the event
-        return true;
-
-      case DragEvent.ACTION_DRAG_EXITED:
-
-        return true;
-
-      case DragEvent.ACTION_DROP:
-
-        ClipData data = event.getClipData();
-        String arrayIndex = (String) data.getItemAt(0).getText();
-        PieceViewList.swapContents(Integer.valueOf(arrayIndex), this.viewArrayIndex);
-        // Returns true. DragEvent.getResult() will return true.
-        return true;
-
-      case DragEvent.ACTION_DRAG_ENDED:
-
-        // returns true; the value is ignored.
-        return true;
-
-      // An unknown action type was received.
-      default:
-        Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
-        break;
-    }
-
-    return false;
-
   }
 
   public void setPieceArrayIndex(int pieceArrayIndex) {
--- app/src/main/java/com/mahsum/puzzle/ui/PuzzlePropertiesDialog.java
+++ app/src/main/java/com/mahsum/puzzle/ui/PuzzlePropertiesDialog.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.mahsum.puzzle.R;
 
 
@@ -20,21 +22,20 @@ public class PuzzlePropertiesDialog extends DialogFragment {
   }
 
   private Uri imageUri;
-
+  @BindView(R.id.piecesX) EditText piecesX;
+  @BindView(R.id.piecesY) EditText piecesY;
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     final LayoutInflater inflater = getActivity().getLayoutInflater();
     final View layout = inflater.inflate(R.layout.puzzle_properties_dialog, null);
+    ButterKnife.bind(this, layout);
 
     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
     builder.setView(layout);
-
     builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
       @Override
       public void onClick(DialogInterface dialog, int which) {
-        final EditText piecesX = (EditText) layout.findViewById(R.id.piecesX);
-        final EditText piecesY = (EditText) layout.findViewById(R.id.piecesY);
         Intent intent = new Intent(inflater.getContext(), GameBoard.class);
         intent.putExtra(GameBoard.ORIGINAL_IMAGE_FILE_PATH, imageUri);
         intent.putExtra(GameBoard.PIECES_X, Integer.valueOf(piecesX.getText().toString()));
@@ -44,7 +45,6 @@ public class PuzzlePropertiesDialog extends DialogFragment {
         startActivity(intent);
       }
     });
-
     builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
       @Override
       public void onClick(DialogInterface dialog, int which) {
